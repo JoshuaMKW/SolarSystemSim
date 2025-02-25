aa20841f0a21f5dd84b1cea0e3b9774eefc5fc93
JoshuaMKW <joshuamkw2002@gmail.com>
2025-02-25 05:17:07 -0600

    Fix bug

--- solarsym/system/body.py
+++ solarsym/system/body.py
@@ -15,7 +15,7 @@ class SystemBody:
 
     # GRAVITY_CONSTANT = 6.67430e-11
     GRAVITY_CONSTANT = 6.67430e3
-    SYSTEM_SCALE = .01
+    SYSTEM_SCALE = 0.01
     COLLISION_DAMPENING = 0.01
 
     _name: str
@@ -31,6 +31,7 @@ class SystemBody:
         """
         Collision information for a body
         """
+
         granularity: int
         bias: float
         impulse: float
@@ -41,10 +42,11 @@ class SystemBody:
         """
         Gravity information for a body
         """
+
         force: float
         direction: list[float, float]
 
-    def __init__(self, name, mass, radius, color, position, velocity = (0, 0)):
+    def __init__(self, name, mass, radius, color, position, velocity=(0, 0)):
         self._name = name
         self._mass = mass
         self._radius = radius
@@ -59,7 +61,7 @@ class SystemBody:
         Get the name of the body
         """
         return self._name
-    
+
     @name.setter
     def name(self, name: str):
         """
@@ -73,77 +75,77 @@ class SystemBody:
         Get the position of the body
         """
         return self._position
-    
+
     @position.setter
     def position(self, position: list[float, float]):
         """
         Set the position of the body
         """
         self._position = position
-    
+
     @property
     def velocity(self) -> list[float, float]:
         """
         Get the velocity of the body
         """
         return self._velocity
-    
+
     @velocity.setter
     def velocity(self, velocity: list[float, float]):
         """
         Set the velocity of the body
         """
         self._velocity = velocity
-    
+
     @property
     def acceleration(self) -> list[float, float]:
         """
         Get the acceleration of the body
         """
         return self._acceleration
-    
+
     @acceleration.setter
     def acceleration(self, acceleration: list[float, float]):
         """
         Set the acceleration of the body
         """
         self._acceleration = acceleration
-    
+
     @property
     def mass(self) -> float:
         """
         Get the mass of the body
         """
         return self._mass
-    
+
     @mass.setter
     def mass(self, mass: float):
         """
         Set the mass of the body
         """
         self._mass = mass
-    
+
     @property
     def radius(self) -> float:
         """
         Get the radius of the body
         """
         return self._radius
-    
+
     @radius.setter
     def radius(self, radius: float):
         """
         Set the radius of the body
         """
         self._radius = radius
-    
+
     @property
     def color(self) -> float:
         """
         Get the color of the body
         """
         return self._color
-    
+
     @color.setter
     def color(self, color: float):
         """
@@ -151,14 +153,23 @@ class SystemBody:
         """
         self._color = color
 
-    def draw(self, screen, delta: float, zoom: float, center: list[float, float], is_hovered: bool = False):
+    def draw(
+        self,
+        screen,
+        delta: float,
+        zoom: float,
+        center: list[float, float],
+        is_hovered: bool = False,
+    ):
         """
         Draw the body on the screen
         """
         # Normalize the postition
         screen_pos = [
-            (self._position[0] - center[0]) * SystemBody.SYSTEM_SCALE * zoom + screen.get_width() / 2,
-            (self._position[1] - center[1]) * SystemBody.SYSTEM_SCALE * zoom + screen.get_height() / 2
+            (self._position[0] - center[0]) * SystemBody.SYSTEM_SCALE * zoom
+            + screen.get_width() / 2,
+            (self._position[1] - center[1]) * SystemBody.SYSTEM_SCALE * zoom
+            + screen.get_height() / 2,
         ]
         screen_radius = self._radius * SystemBody.SYSTEM_SCALE * zoom
 
@@ -204,25 +215,27 @@ class SystemBody:
         """
         dist_x = other.position[0] - self._position[0]
         dist_y = other.position[1] - self._position[1]
-        dist_sqr = dist_x ** 2 + dist_y ** 2
+        dist_sqr = dist_x**2 + dist_y**2
         mass_x = other.mass
         force = (SystemBody.GRAVITY_CONSTANT * mass_x) / dist_sqr
         return force
-    
-    def calc_gravity_at_point(self, point: list[float, float]) -> SystemBody.GravityInfo:
+
+    def calc_gravity_at_point(
+        self, point: list[float, float]
+    ) -> SystemBody.GravityInfo:
         """
         Calculate the gravitational acceleration caused by a point
         """
         dist_x = point[0] - self._position[0]
         dist_y = point[1] - self._position[1]
-        dist_sqr = dist_x ** 2 + dist_y ** 2
+        dist_sqr = dist_x**2 + dist_y**2
+        dist_sqr = max(dist_sqr, self._radius**2)
         dist = math.sqrt(dist_sqr)
         force = (SystemBody.GRAVITY_CONSTANT) / dist_sqr
         return SystemBody.GravityInfo(
-            force=force,
-            direction=[dist_x / dist, dist_y / dist]
+            force=force, direction=[dist_x / dist, dist_y / dist]
         )
-    
+
     def calc_collision_bias(self, other: SystemBody) -> CollisionInfo | None:
         """
         Calculates the bias for collision resolution between 0 and 1
@@ -232,13 +245,13 @@ class SystemBody:
         # Calculate the relative velocity
         rel_vel = [
             self._velocity[0] - other.velocity[0],
-            self._velocity[1] - other.velocity[1]
+            self._velocity[1] - other.velocity[1],
         ]
 
         # Calculate the relative position
         rel_pos = [
             self._position[0] - other.position[0],
-            self._position[1] - other.position[1]
+            self._position[1] - other.position[1],
         ]
 
         rel_pos_mag = rel_pos[0] ** 2 + rel_pos[1] ** 2
@@ -251,16 +264,13 @@ class SystemBody:
             return None
 
         # rel_pos is the normal vector
-        col_normal = [
-            rel_pos[0] / rel_pos_mag,
-            rel_pos[1] / rel_pos_mag
-        ]
-        
+        col_normal = [rel_pos[0] / rel_pos_mag, rel_pos[1] / rel_pos_mag]
+
         # Calculate the relative velocity in the normal direction
         rel_vel_n = rel_vel[0] * col_normal[0] + rel_vel[1] * col_normal[1]
         if rel_vel_n >= 0:
             return None
-        
+
         # Calculate the impulse
         impulse = -2 * rel_vel_n / (1 / self._mass + 1 / other.mass)
         impulse *= other.mass / self._mass
@@ -270,29 +280,25 @@ class SystemBody:
         rel_vel_mag = math.sqrt(rel_vel[0] ** 2 + rel_vel[1] ** 2)
 
         # Calculate the reflection vector
-        rel_vel_normalized = [
-            rel_vel[0] / rel_vel_mag,
-            rel_vel[1] / rel_vel_mag
-        ]
-        
+        rel_vel_normalized = [rel_vel[0] / rel_vel_mag, rel_vel[1] / rel_vel_mag]
+
         reflection = [
             -rel_vel_normalized[0] + 2 * rel_vel_n * col_normal[0],
-            -rel_vel_normalized[1] + 2 * rel_vel_n * col_normal[1]
+            -rel_vel_normalized[1] + 2 * rel_vel_n * col_normal[1],
         ]
 
         # Bias is the density of the body compared to the other body
         bias = self._mass / (self._mass + other.mass)
 
         info = SystemBody.CollisionInfo(
-            granularity=10,
-            bias=bias,
-            impulse=impulse,
-            reflect_dir=reflection
+            granularity=10, bias=bias, impulse=impulse, reflect_dir=reflection
         )
 
         return info
-    
-    def calc_blended_color(self, other: SystemBody, bias: float = 0.5) -> list[float, float, float]:
+
+    def calc_blended_color(
+        self, other: SystemBody, bias: float = 0.5
+    ) -> list[float, float, float]:
         """
         Calculate the blended color of the body with another body
         """
@@ -314,7 +320,7 @@ class SystemBody:
         color = pygame.Color(0)
         color.hsva = (hue, sat, light)
         return color
-    
+
     def _draw_info(self, screen, position: list[float, float], radius: float):
         """
         Draw the body's information on the screen
@@ -326,8 +332,8 @@ class SystemBody:
         # text_rect.y -= (20 + int(radius))
         # screen.blit(text, text_rect)
 
-        text_surf = render_text(f'{self._name} - {self._mass:.2e} kg', PLANET_FONT)
+        text_surf = render_text(f"{self._name} - {self._mass:.2e} kg", PLANET_FONT)
         text_rect = text_surf.get_rect()
         text_rect.center = position
-        text_rect.y -= (20 + int(radius))
+        text_rect.y -= 20 + int(radius)
         screen.blit(text_surf, text_rect)
--- solarsym/system/system.py
+++ solarsym/system/system.py
@@ -12,11 +12,12 @@ from io import BytesIO
 
 from .body import SystemBody
 
+
 class SystemBodyPool:
     """A class to represent a pool of bodies in the solar system"""
 
-    HEATMAP_CELL_SIZE = 10
-    HEATMAP_UPDATE_INTERVAL = 15.0
+    HEATMAP_CELL_SIZE = 7
+    HEATMAP_UPDATE_INTERVAL = 3.0
 
     bodies: list[SystemBody]
     center: list[float, float]
@@ -30,6 +31,7 @@ class SystemBodyPool:
         """
         Information about a collision update
         """
+
         new_color: list[int, int, int]
         new_mass: float
         new_radius: int
@@ -46,7 +48,6 @@ class SystemBodyPool:
             self.affected.mass = self.new_mass
             self.affected.radius = self.new_radius
             self.affected.acceleration = self.due_acceleration
-        
 
     def __init__(self):
         self.bodies = []
@@ -61,18 +62,22 @@ class SystemBodyPool:
         Check if a point intersects any body in the pool
         """
         for body in self.bodies:
-            dist_sqr = (point[0] - body.position[0]) ** 2 + (point[1] - body.position[1]) ** 2
-            if dist_sqr <= body.radius ** 2:
+            dist_sqr = (point[0] - body.position[0]) ** 2 + (
+                point[1] - body.position[1]
+            ) ** 2
+            if dist_sqr <= body.radius**2:
                 return True
 
         return False
-    
+
     def intersects_body(self, body: SystemBody, point: list[float, float]) -> bool:
         """
         Check if a point intersects a specific body in the pool
         """
-        dist_sqr = (point[0] - body.position[0]) ** 2 + (point[1] - body.position[1]) ** 2
-        return dist_sqr <= body.radius ** 2
+        dist_sqr = (point[0] - body.position[0]) ** 2 + (
+            point[1] - body.position[1]
+        ) ** 2
+        return dist_sqr <= body.radius**2
 
     def set_center(self, center: list[float, float]):
         """
@@ -94,19 +99,29 @@ class SystemBodyPool:
 
         click_point = [
             view_pos[0] + (mouse_pos[0] / (SystemBody.SYSTEM_SCALE * zoom)),
-            view_pos[1] + (mouse_pos[1] / (SystemBody.SYSTEM_SCALE * zoom))
+            view_pos[1] + (mouse_pos[1] / (SystemBody.SYSTEM_SCALE * zoom)),
         ]
 
         abs_point = [
-            view_pos[0] + ((mouse_pos[0] + screen.get_width() / 2) / (SystemBody.SYSTEM_SCALE * zoom)),
-            view_pos[1] + ((mouse_pos[1] + screen.get_height() / 2) / (SystemBody.SYSTEM_SCALE * zoom))
+            view_pos[0]
+            + (
+                (mouse_pos[0] + screen.get_width() / 2)
+                / (SystemBody.SYSTEM_SCALE * zoom)
+            ),
+            view_pos[1]
+            + (
+                (mouse_pos[1] + screen.get_height() / 2)
+                / (SystemBody.SYSTEM_SCALE * zoom)
+            ),
         ]
 
-        if (self._heatmap_active and self._heatmap_surface is not None):
+        if self._heatmap_active and self._heatmap_surface is not None:
             screen.blit(self._heatmap_surface, (0, 0))
 
         for body in self.bodies:
-            body.draw(screen, delta, zoom, view_pos, self.intersects_body(body, abs_point))
+            body.draw(
+                screen, delta, zoom, view_pos, self.intersects_body(body, abs_point)
+            )
 
     def move_all(self, delta: float):
         """
@@ -143,39 +158,47 @@ class SystemBodyPool:
                 # Calculate the gravitational acceleration
                 acceleration = affected.calc_gravity_acceleration(affector)
 
-                affect_angle = math.atan2(affected.position[1] - affector.position[1], affected.position[0] - affector.position[0])
+                affect_angle = math.atan2(
+                    affected.position[1] - affector.position[1],
+                    affected.position[0] - affector.position[0],
+                )
 
                 accel_vector = [
                     -acceleration * math.cos(affect_angle),
-                    -acceleration * math.sin(affect_angle)
+                    -acceleration * math.sin(affect_angle),
                 ]
                 affected.add_acceleration(accel_vector)
 
                 # Calculate a collision
                 col_info = affected.calc_collision_bias(affector)
-                if (col_info is None):
+                if col_info is None:
                     continue
 
                 update_info.affectors.append([affector, col_info])
 
-            if (len(update_info.affectors) > 0):
+            if len(update_info.affectors) > 0:
                 pending_updates.append(update_info)
-
 
         for this_update in pending_updates:
             for affector, col_info in this_update.affectors:
-                if (this_update.affected == affector):
+                if this_update.affected == affector:
                     continue
 
                 # Absorb the smaller body
-                if (col_info.bias < 0.1):
+                if col_info.bias < 0.1:
                     bodies_to_remove.add(this_update.affected)
                     continue
-                
+
                 # Merge the bodies if the reflection isn't strong enough
-                reflection_theta = math.atan2(col_info.reflect_dir[1], col_info.reflect_dir[0])
-                if math.fabs(reflection_theta) >= math.pi - 0.05 or math.fabs(reflection_theta) < 0.05 or col_info.impulse < 10:
-                    if (this_update.affected in bodies_to_merge):
+                reflection_theta = math.atan2(
+                    col_info.reflect_dir[1], col_info.reflect_dir[0]
+                )
+                if (
+                    math.fabs(reflection_theta) >= math.pi - 0.05
+                    or math.fabs(reflection_theta) < 0.05
+                    or col_info.impulse < 10
+                ):
+                    if this_update.affected in bodies_to_merge:
                         continue
                     bodies_to_merge.add(this_update.affected)
                     bodies_to_merge.add(affector)
@@ -190,13 +213,17 @@ class SystemBodyPool:
                 this_update.new_radius += affector.radius * (1 - col_info.bias)
 
                 this_update.due_acceleration = [
-                    this_update.due_acceleration[0] + col_info.impulse * col_info.reflect_dir[0],
-                    this_update.due_acceleration[1] + col_info.impulse * col_info.reflect_dir[1]
+                    this_update.due_acceleration[0]
+                    + col_info.impulse * col_info.reflect_dir[0],
+                    this_update.due_acceleration[1]
+                    + col_info.impulse * col_info.reflect_dir[1],
                 ]
 
-                this_update.new_color = this_update.affected.calc_blended_color(affector, col_info.bias)
+                this_update.new_color = this_update.affected.calc_blended_color(
+                    affector, col_info.bias
+                )
 
-                if (col_info.bias >= 0.9):
+                if col_info.bias >= 0.9:
                     continue
 
                 continue
@@ -222,7 +249,7 @@ class SystemBodyPool:
                 #     particle_velocity[1] *= 1.1
 
         for update in pending_updates:
-            if (update.affected in bodies_to_merge):
+            if update.affected in bodies_to_merge:
                 continue
             update.apply()
 
@@ -231,14 +258,25 @@ class SystemBodyPool:
             new_radius = body1.radius + body2.radius
             new_color = body1.calc_blended_color(body2)
             new_position = [
-                (body1.position[0] * body1.mass + body2.position[0] * body2.mass) / new_mass,
-                (body1.position[1] * body1.mass + body2.position[1] * body2.mass) / new_mass
+                (body1.position[0] * body1.mass + body2.position[0] * body2.mass)
+                / new_mass,
+                (body1.position[1] * body1.mass + body2.position[1] * body2.mass)
+                / new_mass,
             ]
             new_velocity = [
-                (body1.velocity[0] * body1.mass + body2.velocity[0] * body2.mass) / new_mass,
-                (body1.velocity[1] * body1.mass + body2.velocity[1] * body2.mass) / new_mass
+                (body1.velocity[0] * body1.mass + body2.velocity[0] * body2.mass)
+                / new_mass,
+                (body1.velocity[1] * body1.mass + body2.velocity[1] * body2.mass)
+                / new_mass,
             ]
-            new_body = SystemBody(f'{body1.name}_{body2.name}', new_mass, new_radius, new_color, new_position, new_velocity)
+            new_body = SystemBody(
+                f"{body1.name}_{body2.name}",
+                new_mass,
+                new_radius,
+                new_color,
+                new_position,
+                new_velocity,
+            )
             bodies_to_remove.add(body1)
             bodies_to_remove.add(body2)
             bodies_to_add.add(new_body)
@@ -261,23 +299,33 @@ class SystemBodyPool:
         """
         if not self._heatmap_active:
             return
-        
-        if (self._heatmap_update_time < SystemBodyPool.HEATMAP_UPDATE_INTERVAL):
+
+        if self._heatmap_update_time < SystemBodyPool.HEATMAP_UPDATE_INTERVAL:
             self._heatmap_update_time += delta
             return
-        
+
         self._heatmap_update_time = 0.0
-        
+
         self._heatmap = []
 
-        for y in range(SystemBodyPool.HEATMAP_CELL_SIZE // 2, view_rect.height + SystemBodyPool.HEATMAP_CELL_SIZE // 2, SystemBodyPool.HEATMAP_CELL_SIZE):
+        for y in range(
+            SystemBodyPool.HEATMAP_CELL_SIZE // 2,
+            view_rect.height + SystemBodyPool.HEATMAP_CELL_SIZE // 2,
+            SystemBodyPool.HEATMAP_CELL_SIZE,
+        ):
             row = []
-            for x in range(SystemBodyPool.HEATMAP_CELL_SIZE // 2, view_rect.width + SystemBodyPool.HEATMAP_CELL_SIZE // 2, SystemBodyPool.HEATMAP_CELL_SIZE):
+            for x in range(
+                SystemBodyPool.HEATMAP_CELL_SIZE // 2,
+                view_rect.width + SystemBodyPool.HEATMAP_CELL_SIZE // 2,
+                SystemBodyPool.HEATMAP_CELL_SIZE,
+            ):
                 point = [
-                    (x - view_rect.width // 2) / (SystemBody.SYSTEM_SCALE * scale) + view_rect.x,
-                    (y - view_rect.height // 2) / (SystemBody.SYSTEM_SCALE * scale) + view_rect.y,
+                    (x - view_rect.width // 2) / (SystemBody.SYSTEM_SCALE * scale)
+                    + view_rect.x,
+                    (y - view_rect.height // 2) / (SystemBody.SYSTEM_SCALE * scale)
+                    + view_rect.y,
                 ]
-                
+
                 total_accel = 0
                 for body in self.bodies:
                     new_info = body.calc_gravity_at_point(point)
@@ -325,7 +373,9 @@ class SystemBodyPool:
         if (accel_min - accel_max) == 0:
             return surface
 
-        data_range = math.log(accel_max / accel_min)
+        accel_max = math.log(accel_max)
+        accel_min = math.log(accel_min)
+        data_range = accel_max - accel_min
 
         accel_max /= data_range
         accel_min /= data_range
@@ -334,7 +384,7 @@ class SystemBodyPool:
 
         for y, row in enumerate(self._heatmap):
             for x, value in enumerate(row):
-                value = ((math.log(value) / data_range - accel_min) / scaled_range)
+                value = (math.log(value) / data_range - accel_min) / scaled_range
                 color = int(value * 255)
                 pygame.draw.rect(
                     surface,
@@ -343,8 +393,8 @@ class SystemBodyPool:
                         x * SystemBodyPool.HEATMAP_CELL_SIZE,
                         y * SystemBodyPool.HEATMAP_CELL_SIZE,
                         SystemBodyPool.HEATMAP_CELL_SIZE,
-                        SystemBodyPool.HEATMAP_CELL_SIZE
-                    )
+                        SystemBodyPool.HEATMAP_CELL_SIZE,
+                    ),
                 )
 
         return surface
